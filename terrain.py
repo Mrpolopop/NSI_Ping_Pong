130e09c7709c030a70a8d6e3d18dfbd5700c179f
MatthMad <77395101+MatthMad@users.noreply.github.com>
2024-04-12 09:04:35 +0200

    Update terrain.py
    Service départ

--- terrain.py
+++ terrain.py
@@ -1,7 +1,10 @@
 from microbit import *
 import neopixel
 import time
+from random import randint
+from lcd_i2c import LCD1602
 
+lcd = LCD1602()
 NP_LED_COUNT_0 = 30
 
 # Neopixel on pin0
@@ -9,59 +12,70 @@ np_0 = neopixel.NeoPixel(pin0, NP_LED_COUNT_0)
 
 
 def terrain():
+    led_rouge = [0, 29]
+    led_orange = [1, 2, 27, 28]
+    led_verte = [3, 4, 5, 24, 25, 26]
+
+    for led in led_rouge:
+        np_0[led] = (255, 0, 0)
+
+    for led in led_orange:
+        np_0[led] = (255, 130, 0)
+
+    for led in led_verte:
+        np_0[led] = (0, 255, 0)
+
+    np_0.show()
+    
+
+#def ecran_lcd():
   
-  led_rouge = [0,29]
-  led_orange = [1, 2, 27, 28]
-  led_verte = [3, 4, 5, 24, 25, 26]
-  
-  for led in led_rouge:
-    np_0[led] = (255, 0, 0)
+#  lcd.clear()
+#  lcd.getCursor(0, 0)
+#  lcd.writeTxt('Joueur 1')
   
 
-  for led in led_orange:
-    np_0[led] = (220, 100, 0)
-  
+
+def balle_gauche(delta, start):
+    for i in range(start, NP_LED_COUNT_0):
+        color = np_0[i]
+        np_0[i] = (200, 200, 200)
+        np_0.show()
+        time.sleep(delta)
+        np_0[i] = color
+        np_0.show()
 
 
-  for led in led_verte:
-    np_0[led] = (0, 255, 0)
-  
-  np_0.show()
-  
+def balle_droite(delta, start):
+    for i in range(start, -1, -1):
+        color = np_0[i]
+        np_0[i] = (200, 200, 200)
+        np_0.show()
+        time.sleep(delta)
+        np_0[i] = color
+        np_0.show()
 
 
 def eteindre(temps):
-  time.sleep(temps)
-  
-  for i in range(NP_LED_COUNT_0):
-    np_0[i] = (0, 0, 0)
-  
-  np_0.show()
-  
-def balle_gauche(delta, start):
-  
-  for i in range(start, NP_LED_COUNT_0):
-    
-    color = np_0[i]
-    np_0[i] = (200, 200, 200)
-    np_0.show()
-    time.sleep(delta)
-    np_0[i] = color
-    np_0.show()
-    
+    time.sleep(temps)
 
-def balle_droite(delta, start):
-  
-  for i in range(start, -1, -1):
-    
-    color = np_0[i]
-    np_0[i] = (200, 200, 200)
-    np_0.show()
-    time.sleep(delta)
-    np_0[i] = color
+    for i in range(NP_LED_COUNT_0):
+        np_0[i] = (0, 0, 0)
+
     np_0.show()
 
-terrain()
-balle_gauche(0.1, 12)
-balle_droite(0.1, 20)
-eteindre(1)
+
+def service_depart():
+  nombre = randint(1,2)
+  if nombre == 1:
+    balle_droite(0.15, 14)
+  else:
+    balle_gauche(0.15, 14)
+
+
+if __name__ == "__main__":
+  terrain()
+  ecran_lcd()
+  service_depart()
+  eteindre(60)
+  
