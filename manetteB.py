7e398ff89176f76f3f4bd5e8ab74c9692ca79a0e
Mrpolopop <134200767+Mrpolopop@users.noreply.github.com>
2024-05-17 10:01:51 +0200

    Update manetteB.py

--- manetteB.py
+++ manetteB.py
@@ -1,11 +1,13 @@
 from microbit import *
 import radio
+import time
 
 radio.on()
 radio.config(channel = 12, power = 6, length = 32, group=12)
 
 display.show("D")
-
 while True:
   if button_a.is_pressed():
     radio.send("bouton_B")
+    time.sleep(0.2)
+    radio.send("null")
